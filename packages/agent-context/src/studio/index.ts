dfb2c1424374aad2d3e2f58f81997ca1aa66b255
Herman Wikner <wiknerherman@gmail.com>
2026-02-03 12:48:02 +0100

    feat(packages/agent-context): agent context plugin improvements (#3)
    * feat(packages/agent-context): agent context plugin updates
    
    * fix: updates

--- packages/agent-context/src/studio/index.ts
+++ packages/agent-context/src/studio/index.ts
@@ -2,10 +2,5 @@
 export {
   AGENT_CONTEXT_SCHEMA_TYPE_NAME,
   agentContextSchema,
-} from './context-plugin/agentContextSchema'
-export {agentContextPlugin} from './context-plugin/plugin'
-
-/**
- * @deprecated Use `agentContextPlugin` instead. Will be removed in next major version.
- */
-export {agentContextPlugin as contextPlugin} from './context-plugin/plugin'
+} from './agent-context-plugin/agentContextSchema'
+export {agentContextPlugin} from './agent-context-plugin/plugin'
